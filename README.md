f1a38f472605bde8b481a897587138759a0dbbaf
Lilisys <jesswu@umich.edu>
2016-06-17 11:49:24 -0400

    basic api query working

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # HFHackDay1
 Hacker Fellows 2016 Grand Circus Bootcamp Hack Day June 17th
 
+The task is to ingest an API, persist data in a database, and allow for deletion in the db (and on the screen) via a command from the webpage. Please use Postgres for the database and jQuery on the front-end.
+
+The details: the page should have a button that pulls in new data from the API and puts it in the database. For example, you might query a movies API for your favorite movie and add specific information on that movie to your database. Include an edit button so that you can update each record with your own notes. The page should have a table that shows the data in the db and a delete button to delete a row. The calls should be made using ajax.
+
 ## Info on RESTful:
 - when you send something, it won't modify sender or reciever server
 - It's stateless (nothing gets saved)
--- backend.js
+++ backend.js
@@ -1,4 +1,37 @@
-$("#pokeform").submit(function(event){
-	alert("omg");
-	event.preventDefault();
-})
+$("#submitButton").click(function(event){
+	apiQuery(String($("#userIn").val()));
+});
+
+function apiQuery(pokemon){
+	var apiCall = "http://pokeapi.co/api/v2/pokemon/" + pokemon;
+	console.log(apiCall);
+	$.get(apiCall, function(response) {
+		postAPI(response);
+	});
+};
+
+function postAPI(response){
+	console.log(response);
+	var name = response["name"];
+	var rtypes = response["types"];
+	console.log("rtypes", rtypes)
+	var types = {};
+
+	//parse out the types
+	for (var i = 0; i < rtypes.length; ++i) {
+		types[rtypes[i]["slot"]] = rtypes[i]["type"]["name"];
+	};
+
+	if(!types[2]){
+		types[2] = "N/A";
+	}
+
+	var htmlString = "<tr><td>" + name + "</td><td>" + types[1] +"</td><td>" + types[2] + "</td></tr>"
+	$('#pokeTable').append(htmlString);
+	$('#pokeTable').show();
+}
+
+// var apiCallTest = "http://pokeapi.co/api/v2/pokemon/mudkip";
+// $.get(apiCallTest, function(response) {
+// 		console.log(response["name"]);
+// 	});	
